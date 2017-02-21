d54f7a769ccb60bd5feaab46a68d636dab38b02b
BeatButton <thebeatbutton@gmail.com>
2017-02-20 22:23:07 -0700

    Set up logging as per documentation example.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
+import logging
 
+import discord
 from discord.ext.commands import when_mentioned_or
 
 from bot import BeattieBot
@@ -24,5 +26,11 @@ for extension in ('default', 'rpg', 'eddb', 'repl'):
         bot.load_extension(extension)
     except Exception as e:
         print(f'Failed to load extension {extension}\n{type(e).__name__}: {e}')
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 bot.run(token)
