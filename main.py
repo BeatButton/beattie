4998dd7cd9c97821071b6d06edc95b87184f4c42
BeatButton <thebeatbutton@gmail.com>
2017-02-23 22:33:47 -0700

    Changed default log level to INFO

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ for extension in ('default', 'rpg', 'eddb', 'repl'):
         print(f'Failed to load extension {extension}\n{type(e).__name__}: {e}')
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
