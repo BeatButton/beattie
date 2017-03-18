700089be3f8f4daf78967c7c91a8cdf2501552ed
BeatButton <thebeatbutton@gmail.com>
2017-03-17 21:15:08 -0600

    Fix eating exceptions, add more self-bot support to context, a reload comment baked into the bot

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext.commands import Bot, Context, errors
 from aiohttp import ClientSession
 
+from utils import checks, contextmanagers
+
 
 class BContext(Context):
     async def reply(self, content, sep='\n'):
@@ -9,19 +11,39 @@ class BContext(Context):
             content = f'{self.author.mention}{sep}{content}'
         return await self.send(content)
 
-    async def send(self, content=None, **kwargs):
+    async def send(self, content=None, *, embed=None, **kwargs):
         if self.me.bot:
-            return await super().send(content, **kwargs)
+            return await super().send(content, embed=embed, **kwargs)
         elif content:
             content = f'{self.message.content}\n{content}'
             await self.message.edit(content=content)
             return self.message
+        elif embed:
+            await self.message.delete()
+            return await super().send(embed=embed, **kwargs)
+
+    def typing(self):
+        if self.me.bot:
+            return super().typing()
+        else:
+            return contextmanagers.null()
 
 
 class BeattieBot(Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.session = ClientSession(loop=self.loop)
+
+        @self.command(hidden=True)
+        @checks.is_owner()
+        async def reload(ctx, *, cog):
+            try:
+                self.unload_extension(cog)
+                self.load_extension(cog)
+            except Exception as e:
+                await ctx.send(f'{type(e).__name__}: {e}')
+            else:
+                await ctx.send('Reload successful.')
 
     def __del__(self):
         self.session.close()
@@ -33,17 +55,12 @@ class BeattieBot(Bot):
             await ctx.send('Bad arguments.')
         elif isinstance(e, errors.CheckFailure):
             await ctx.send('You lack the required permissions.')
-        elif isinstance(e, errors.CommandInvokeError):
-            if (isinstance(e.original, discord.errors.HTTPException)
-               and e.original.response.status == 400):
+        elif (isinstance(e, errors.CommandInvokeError)
+              and (isinstance(e.original, discord.errors.HTTPException)
+              and e.original.response.status == 400)):
                 await ctx.send('Message content too long.')
         elif not isinstance(e, errors.CommandNotFound):
-            await ctx.send('Generic error handler triggered. '
-                           'This should never happen.')
-            try:
-                raise e.original
-            except AttributeError:
-                raise e
+            await ctx.send(f'{type(e).__name__}: {e}')
 
     async def on_ready(self):
         print('Logged in as')
