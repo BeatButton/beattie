dcb2c88d79de1dbe610424b7e522e12126ea9734
BeatButton <thebeatbutton@gmail.com>
2017-03-23 15:56:32 -0600

    Moved xkcd commands into their own cog.

--- default.py
+++ default.py
@@ -1,12 +1,7 @@
-from codecs import encode
-from json import JSONDecodeError
-import random
-import re
 import time
 
-import discord
 from discord.ext import commands
-from lxml import etree
+import yaml
 
 from utils import checks
 
@@ -14,8 +9,64 @@ from utils import checks
 class Default:
     def __init__(self, bot):
         self.bot = bot
-        with open('data/why.txt', encoding='utf8') as file:
-            self.questions = tuple(file.readlines())
+        try:
+            with open('cog_blacklist.yaml') as file:
+                self.bot.cog_blacklist = yaml.load(file)
+        except FileNotFoundError:
+            self.bot.cog_blacklist = {}
+        else:
+            if self.bot.cog_blacklist is None:
+                self.bot.cog_blacklist = {}
+
+    def __global_check(self, ctx):
+        if checs.is_owner_check(ctx):
+            return True
+        cog = ctx.command.cog_name
+        return cog not in self.bot.cog_blacklist.get(ctx.guild.id, set())
+
+    def _update_blacklist(self):
+        with open('cog_blacklist.yaml', 'w') as file:
+            yaml.dump(self.bot.cog_blacklist, file)
+
+    @commands.command(hidden=True)
+    @checks.is_owner()
+    async def reload(ctx, *, cog):
+        self.unload_extension(cog)
+        self.load_extension(cog)
+        await ctx.send('Reload successful.')
+
+    @commands.command()
+    async def enable(self, ctx, cog):
+        """Enable a cog in the guild."""
+        if self.bot.get_cog(cog) is None:
+            await ctx.send("That cog doesn't exist.")
+        else:
+            blacklist = self.bot.cog_blacklist.get(ctx.guild.id, set())
+            try:
+                blacklist.remove(cog)
+            except KeyError:
+                await ctx.send('Cog is already enabled.')
+            else:
+                self.bot.cog_blacklist[ctx.guild.id] = blacklist
+                await ctx.send('Cog enabled for this guild.')
+                self._update_blacklist()
+
+    @commands.command()
+    async def disable(self, ctx, cog):
+        """Disable a cog in the guild."""
+        if self.bot.get_cog(cog) is None:
+            try:
+                self.bot.load_extension(cog)
+            except ModuleNotFoundError:
+                pass
+        if self.bot.get_cog(cog) is None:
+            await ctx.send("That cog doesn't exist.")
+        else:
+            blacklist = self.bot.cog_blacklist.get(ctx.guild.id, set())
+            blacklist.add(cog)
+            self.bot.cog_blacklist[ctx.guild.id] = blacklist
+            await ctx.send('Cog disabled for this guild.')
+            self._update_blacklist()
 
     @commands.command(aliases=['p'])
     async def ping(self, ctx):
@@ -28,118 +79,6 @@ class Default:
         ping_time = (after - before) * 1000
 
         await msg.edit(content=f'{msg.content} **{ping_time:.0f}ms**')
-
-    @commands.command()
-    async def choose(self, ctx, *options):
-        """Choose between some options. Use quotes if they have spaces."""
-        await ctx.send(random.choice(options))
-
-    @commands.command()
-    async def why(self, ctx):
-        """Asks a question."""
-        await ctx.send(random.choice(self.questions))
-
-    @commands.group()
-    async def xkcd(self, ctx, *, inp=None):
-        """Commands for getting xkcd comics"""
-        async with ctx.typing():
-            url = 'https://xkcd.com/info.0.json'
-            async with self.bot.session.get(url) as resp:
-                self.xkcd_data = await resp.json()
-            if inp == 'random':
-                await ctx.invoke(self.random)
-            elif inp in ('latest', 'current'):
-                await ctx.invoke(self.latest)
-            else:
-                if inp:
-                    await ctx.invoke(self.comic, inp=inp)
-                else:
-                    await ctx.invoke(self.random)
-
-    @xkcd.command()
-    async def random(self, ctx):
-        """Gets a random xkcd comic."""
-        await ctx.invoke(self.comic,
-                         inp=random.randint(1, self.xkcd_data['num']))
-
-    @xkcd.command()
-    async def latest(self, ctx):
-        """Gets the latest xkcd comic."""
-        await ctx.send(embed=format_comic(self.xkcd_data))
-
-    @xkcd.command()
-    async def comic(self, ctx, *, inp):
-        """Gets an xkcd comic by number or content."""
-        try:
-            number = int(inp)
-        except ValueError:
-            url = 'https://duckduckgo.com/html/'
-            params = {'q': f'{inp} xkcd'}
-            async with self.bot.session.get(url, params=params) as resp:
-                text = await resp.text()
-            match = re.search(r'xkcd\.com/(\d+)', text)
-            if match:
-                number = int(match.groups()[0])
-            else:
-                await ctx.send('No comic found.')
-                return
-
-        if number > self.xkcd_data['num']:
-            await ctx.send('No comic found.')
-            return
-
-        url = f'https://xkcd.com/{number}/info.0.json'
-        async with self.bot.session.get(url) as resp:
-            try:
-                data = await resp.json()
-            except JSONDecodeError:
-                data = {'title': '404',
-                        'img': 'http://www.explainxkcd.com/wiki/'
-                               'images/9/92/not_found.png',
-                        'alt': 'Comic not found.',
-                        'num': 404,
-                        }
-        await ctx.send(embed=format_comic(data))
-
-    @commands.group(aliases=['str', 's'])
-    async def string(self, ctx):
-        """Commands for doing stuff to strings."""
-        if ctx.invoked_subcommand is None:
-            await ctx.send('Invalid command passed. '
-                           f'Try "{ctx.prefix}help string"')
-
-    @string.command(aliases=['rev', 'r'])
-    async def reverse(self, ctx, *, inp):
-        """Reverse a string."""
-        await ctx.send(inp[::-1])
-
-    @string.command(aliases=['rot'])
-    async def rot13(self, ctx, *, inp):
-        """Apply rot13 to a string.
-
-        Uses the shift cipher with key 13."""
-        await ctx.send(encode(inp, 'rot_13'))
-
-    @commands.command(hidden=True)
-    @checks.is_owner()
-    async def sudo(self, ctx, *, inp):
-        await ctx.send('Operation successful.')
-
-    @sudo.error
-    async def sudo_error(self, exception, ctx):
-        if isinstance(exception, commands.errors.CheckFailure):
-            await ctx.send('Unable to lock /var/lib/dpkg/, are you root?')
-        else:
-            await self.bot.handle_error(exception, ctx)
-
-
-def format_comic(data):
-    embed = discord.Embed()
-    embed.title = data['title']
-    embed.set_image(url=data['img'])
-    embed.set_footer(text=data['alt'])
-    embed.url = f"https://www.xkcd.com/{data['num']}/"
-    return embed
 
 
 def setup(bot):
---
+++ xkcd.py
@@ -0,0 +1,108 @@
+from json import JSONDecodeError
+import re
+
+import discord
+from discord.ext import commands
+
+
+class XKCD:
+    def __init__(self, bot):
+        self.bot = bot
+        with open('data/why.txt', encoding='utf8') as file:
+            self.questions = tuple(file.readlines())
+
+    @commands.command()
+    async def why(self, ctx):
+        """Asks a question."""
+        await ctx.send(random.choice(self.questions))
+
+    @commands.group()
+    async def xkcd(self, ctx, *, inp=None):
+        """Commands for getting xkcd comics"""
+        async with ctx.typing():
+            url = 'https://xkcd.com/info.0.json'
+            async with self.bot.session.get(url) as resp:
+                self.xkcd_data = await resp.json()
+            if inp == 'random':
+                await ctx.invoke(self.random)
+            elif inp in ('latest', 'current'):
+                await ctx.invoke(self.latest)
+            else:
+                if inp:
+                    await ctx.invoke(self.comic, inp=inp)
+                else:
+                    await ctx.invoke(self.random)
+
+    @xkcd.command()
+    async def random(self, ctx):
+        """Gets a random xkcd comic."""
+        await ctx.invoke(self.comic,
+                         inp=random.randint(1, self.xkcd_data['num']))
+
+    @xkcd.command()
+    async def latest(self, ctx):
+        """Gets the latest xkcd comic."""
+        await ctx.send(embed=format_comic(self.xkcd_data))
+
+    @xkcd.command()
+    async def comic(self, ctx, *, inp):
+        """Gets an xkcd comic by number or content."""
+        try:
+            number = int(inp)
+        except ValueError:
+            url = 'https://duckduckgo.com/html/'
+            params = {'q': f'{inp} xkcd'}
+            async with self.bot.session.get(url, params=params) as resp:
+                text = await resp.text()
+            match = re.search(r'xkcd\.com/(\d+)', text)
+            if match:
+                number = int(match.groups()[0])
+            else:
+                await ctx.send('No comic found.')
+                return
+
+        if number > self.xkcd_data['num']:
+            await ctx.send('No comic found.')
+            return
+
+        url = f'https://xkcd.com/{number}/info.0.json'
+        async with self.bot.session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except JSONDecodeError:
+                data = {'title': '404',
+                        'img': 'http://www.explainxkcd.com/wiki/'
+                               'images/9/92/not_found.png',
+                        'alt': 'Comic not found.',
+                        'num': 404,
+                        }
+        await ctx.send(embed=format_comic(data))
+
+    @commands.command(hidden=True)
+    @checks.is_owner()
+    async def sudo(self, ctx, *, inp):
+        await ctx.send('Operation successful.')
+
+    @sudo.error
+    async def sudo_error(self, e, ctx):
+        try:
+            e = e.original
+        except AttributeError:
+            pass
+        if isinstance(env, commands.errors.CheckFailure):
+            await ctx.send('Unable to lock /var/lib/dpkg/, are you root?')
+        else:
+            await self.bot.handle_error(e, ctx)
+
+
+def format_comic(data):
+    embed = discord.Embed()
+    embed.title = data['title']
+    embed.set_image(url=data['img'])
+    embed.set_footer(text=data['alt'])
+    embed.url = f"https://www.xkcd.com/{data['num']}/"
+    return embed
+
+
+def setup(bot):
+    bot.add_cog(XKCD(bot))
