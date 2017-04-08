c062e7caed7a01a0d4ba7a44b6ed1f8326b79808
BeatButton <thebeatbutton@gmail.com>
2017-04-08 16:35:53 -0600

    Add rule34.xxx searching, refactor booru API access into its own function

--- nsfw.py
+++ nsfw.py
@@ -12,26 +12,13 @@ class NSFW:
     @commands.command(aliases=['gel'], hidden=True)
     async def gelbooru(self, ctx, *, tags):
         async with ctx.typing():
-            entries = []
-            url = 'http://gelbooru.com/index.php'
-            params = {'page': 'dapi',
-                      's': 'post',
-                      'q': 'index',
-                      'tags': tags}
-            async with self.bot.get(url, params=params) as resp:
-                root = etree.fromstring((await resp.text()).encode(),
-                                        etree.HTMLParser())
-            search_nodes = root.findall(".//post")
-            for node in search_nodes:
-                image = next((item[1] for item in node.items()
-                             if item[0] == 'file_url'), None)
-                if image is not None:
-                    entries.append(image)
-            try:
-                url = f'http:{random.choice(entries)}'
-            except IndexError:
-                await ctx.send('No images found.')
-                return
+            url = await self.booru('http://gelbooru.com/index.php', tags)
+            await ctx.send(url)
+
+    @commands.command(aliases=['r34'], hidden=True)
+    async def rule34(self, ctx, *, tags):
+        async with ctx.typing():
+            url = await self.booru('http://rule34.xxx/index.php', tags)
             await ctx.send(url)
 
     @commands.command(hidden=True)
@@ -45,6 +32,26 @@ class NSFW:
                   'vaginal', 'futa', 'futanari', 'bisexual']
         tags = ' '.join([f'-{tag}' for tag in ignore] + [tags])
         await ctx.invoke(self.gelbooru, tags=f'shota {tags}')
+
+    async def booru(self, url, tags):
+        entries = []
+        params = {'page': 'dapi',
+                  's': 'post',
+                  'q': 'index',
+                  'tags': tags}
+        async with self.bot.get(url, params=params) as resp:
+            root = etree.fromstring((await resp.text()).encode(),
+                                    etree.HTMLParser())
+        search_nodes = root.findall(".//post")
+        for node in search_nodes:
+            image = next((item[1] for item in node.items()
+                         if item[0] == 'file_url'), None)
+            if image is not None:
+                entries.append(image)
+        try:
+            return f'http:{random.choice(entries)}'
+        except IndexError:
+            return 'No images found.'
 
 
 def setup(bot):
