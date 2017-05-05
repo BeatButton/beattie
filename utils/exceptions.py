f057a3db4af45b05919afa1e0ba9634fcb931373
BeatButton <thebeatbutton@gmail.com>
2017-05-05 00:05:43 -0600

    Add docstrings

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,4 +1,5 @@
 class ResponseError(Exception):
+    """For throwing in case of a non-200 response status."""
     def __init__(self, *args, code=None, **kwargs):
         self.code = code
         super().__init__(*args, **kwargs)
