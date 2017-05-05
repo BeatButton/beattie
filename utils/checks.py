63c7c76456079d61c8d837a0809ee3b7f75ddbe7
Mitchell Ferree <thebeatbutton@gmail.com>
2017-05-05 00:04:58 -0600

    Make is_owner_or not return True always

--- utils/checks.py
+++ utils/checks.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 def is_owner_or(**perms):
     async def predicate(ctx):
-        owner = ctx.bot.is_owner(ctx.author)
+        owner = await ctx.bot.is_owner(ctx.author)
         permissions = ctx.channel.permissions_for(ctx.author)
         return all(getattr(permissions, perm, None) == value
                    for perm, value in perms.items()) or owner
