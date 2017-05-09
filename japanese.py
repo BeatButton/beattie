45f2aef68a7f6d6f65eddf4b40764a3e8792a9c4
Mitchell Ferree <thebeatbutton@gmail.com>
2017-05-09 02:42:55 -0600

    Handle when lookup fails

--- japanese.py
+++ japanese.py
@@ -14,16 +14,24 @@ class Japanese:
     async def lookup(self, ctx, *, keyword):
         """Get results from Jisho.org, Japanese dictionary"""
         data = await self.jisho.lookup(keyword)
+        if not data:
+            await ctx.send('No words found.')
+            return
         res = data[0]
         res = {k: '\n'.join(v) for k, v in res.items()}
         embed = discord.Embed()
         embed.url = self.search_url.format(keyword)
         embed.title = keyword
+        res = {k: self.format(v) for k, v in res.items()}
         embed.add_field(name='Words', value=res['words'])
         embed.add_field(name='Readings', value=res['readings'])
         embed.add_field(name='Parts of Speech', value=res['parts_of_speech'])
         embed.add_field(name='Meanings', value=res['english'])
         await ctx.send(embed=embed)
+
+    @staticmethod
+    def format(val):
+        return val if val else 'None'
 
 
 def setup(bot):
