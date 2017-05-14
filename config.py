886ce2295f2d363d2a2c6358b1ed5c826c4ac6d5
Mitchell Ferree <thebeatbutton@gmail.com>
2017-05-14 00:21:58 -0600

    Change bad references to self.conn to self.pool, only be in the context manager when necessary

--- config.py
+++ config.py
@@ -20,28 +20,28 @@ class Config:
         self.bot.loop.create_task(self.db.close())
 
     async def get(self, gid):
+        query = 'SELECT * FROM guild WHERE id = $1;'
+        args = (gid,)
         async with self.pool.acquire() as conn:
-            query = 'SELECT * FROM guild WHERE id = $1;'
-            args = (gid,)
             guild = await conn.fetchrow(query, *args)
-            return dict(guild.items())
+        return dict(guild.items())
 
     async def set(self, gid, **kwargs):
+        fmt = ', '.join(f'{k} = ${i}' for i, k in enumerate(kwargs, 2))
+        query = f'UPDATE guild SET {fmt} WHERE id = $1;'
+        args = (gid, *kwargs.values())
         async with self.pool.acquire() as conn:
-            fmt = ', '.join(f'{k} = ${i}' for i, k in enumerate(kwargs, 2))
-            query = f'UPDATE guild SET {fmt} WHERE id = $1;'
-            args = (gid, *kwargs.values())
             await conn.execute(query, *args)
 
     async def add(self, gid, **kwargs):
-        async with self.conn.acquire() as conn:
-            kwargs['id'] = gid
-            fmt = ', '.join(f'{k} = ${i}' for i, k in enumerate(kwargs))
-            query = f'INSERT INTO guild VALUES ({fmt});'
+        kwargs['id'] = gid
+        fmt = ', '.join(f'{k} = ${i}' for i, k in enumerate(kwargs))
+        query = f'INSERT INTO guild VALUES ({fmt});'
+        async with self.pool.acquire() as conn:
             await conn.execute(query, *kwargs.values())
 
     async def remove(self, gid):
-        async with self.conn.acquire() as conn:
-            query = 'DELETE FROM guild WHERE id = $1;'
-            args = (gid,)
+        query = 'DELETE FROM guild WHERE id = $1;'
+        args = (gid,)
+        async with self.pool.acquire() as conn:
             await conn.execute(query, *args)
