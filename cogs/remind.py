1d85b8750f455ab26adab8713da39a5e561c663a
Mitchell Ferree <thebeatbutton@gmail.com>
2017-06-03 21:37:17 -0600

    Add reminder function

---
+++ cogs/remind.py
@@ -0,0 +1,86 @@
+import asyncio
+from bisect import insort_left
+from collections import namedtuple
+from datetime import datetime
+import operator
+
+import asyncpg
+from discord.ext import commands
+import yaml
+
+from utils.converters import Time
+
+
+Task = namedtuple('Task', 'time channel message')
+
+
+class Remind:
+    def __init__(self, bot):
+        with open('config/config.yaml') as file:
+            data = yaml.load(file)
+        self.password = data.get('config_password', '')
+        self.bot = bot
+        self.queue = []
+        self.bot.loop.create_task(self.init())
+
+    async def init(self):
+        self.pool = await asyncpg.create_pool(user='beattie',
+                                              password=self.password,
+                                              database='schedule',
+                                              host='localhost')
+        async with self.pool.acquire() as conn:
+            query = 'SELECT * from message;'
+            for record in await conn.fetch(query):
+                insort_left(self.queue, Task(*record))
+        await self.start_timer()
+
+    @commands.command()
+    async def remind(self, ctx, *, inp):
+        try:
+            time, topic = (i.strip() for i in inp.split(','))
+        except ValueError:
+            time, topic = inp, 'something'
+        time = await Time().convert(ctx, time)
+        message = (f'{ctx.author.mention}\n'
+                   f'You asked to be reminded about {topic}.')
+        await self.schedule_message(ctx.channel.id, message, time)
+
+    async def schedule_message(self, channel, message, time):
+        args = (channel, message, time)
+        async with self.pool.acquire() as conn:
+            query = 'INSERT INTO message VALUES($1, $2, $3);'
+            await conn.execute(query, *args)
+        if self.queue:
+            old = self.queue[0]
+        else:
+            old = None
+        task = Task(*args)
+        insort_left(self.queue, task)
+        if old is not self.queue[0]:
+            if self.timer:
+                self.timer.cancel()
+            await self.start_timer()
+
+    async def send_message(self, task):
+        channel = self.bot.get_channel(task.channel)
+        await channel.send(task.message)
+        async with self.pool.acquire() as conn:
+            query = ('DELETE FROM message WHERE '
+                     'channel = $1 AND message = $2 AND time = $3;')
+            await conn.execute(query, *task)
+
+    async def start_timer(self):
+        self.timer = self.bot.loop.create_task(self.sleep())
+
+    async def sleep(self):
+        while self.queue:
+            delta = (self.queue[0].time - datetime.now()).total_seconds()
+            if delta <= 0:
+                await self.send_message(self.queue[0])
+                del self.queue[0]
+            else:
+                await asyncio.sleep(min(delta, 1_000_000))
+
+
+def setup(bot):
+    bot.add_cog(Remind(bot))
---
+++ utils/converters.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import re
+from time import mktime
+
+from discord.ext.commands import BadArgument, Converter
+import parsedatetime
+
+
+class Time(Converter):
+    def __init__(self):
+        self.cal = parsedatetime.Calendar()
+
+    async def convert(self, ctx, argument):
+        time, code = self.cal.parse(argument)
+        if code == 0:
+            raise BadArgument
+        return datetime.fromtimestamp(mktime(time))
