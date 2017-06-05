474676aaf83aead8a22ce60021b6ca909ad25704
Mitchell Ferree <thebeatbutton@gmail.com>
2017-06-04 18:08:11 -0600

    Use one pool for everything

--- config.py
+++ config.py
@@ -4,24 +4,14 @@ import yaml
 
 class Config:
     def __init__(self, bot):
-        with open('config/config.yaml') as file:
-            data = yaml.load(file)
-        self.password = data.get('config_password', '')
-        self.bot = bot
-        self.bot.loop.create_task(self._create_pool())
-
-    async def _create_pool(self):
-        self.pool = await asyncpg.create_pool(user='beattie',
-                                              password=self.password,
-                                              database='config',
-                                              host='localhost')
+        self.pool = bot.pool
 
     async def get(self, gid):
         query = 'SELECT * FROM guild WHERE id = $1;'
         args = (gid,)
         async with self.pool.acquire() as conn:
             guild = await conn.fetchrow(query, *args)
-        return dict(guild.items())
+        return {k: v for k, v in guild.items() if v}
 
     async def set(self, gid, **kwargs):
         fmt = ', '.join(f'{k} = ${i}' for i, k in enumerate(kwargs, 2))
