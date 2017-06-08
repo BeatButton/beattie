d676a1b1e7e3efbbfc72f1d7e522865b623783df
BeatButton <thebeatbutton@gmail.com>
2017-06-08 08:19:52 -0600

    Add optional hi and lo params to reverse_insort

--- utils/etc.py
+++ utils/etc.py
@@ -1,6 +1,6 @@
-def reverse_insort(seq, val):
-    lo = 0
-    hi = len(seq)
+def reverse_insort(seq, val, lo=0, hi=None):
+    if hi is None:
+        hi = len(seq)
     while lo < hi:
         mid = (lo + hi) // 2
         if val > seq[mid]:
