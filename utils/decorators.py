57f5d8c1f02aa5d3a1be5fdc7302a15d416071dc
BeatButton <thebeatbutton@gmail.com>
2017-06-13 16:39:10 -0600

    Make bot_only async, because duh

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,10 +1,10 @@
 from functools import wraps
 
 
-def bot_only(func):
-    @wraps(func)
-    def inner(self, *args, **kwargs):
+def bot_only(coro):
+    @wraps(coro)
+    async def inner(self, *args, **kwargs):
         if not self.user.bot:
             return
-        return func(self, *args, **kwargs)
+        return await coro(self, *args, **kwargs)
     return inner
