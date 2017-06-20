6e63b00d61bcafbbbbc2a49b8c087677fca1e70f
Mitchell Ferree <thebeatbutton@gmail.com>
2017-06-19 21:45:20 -0600

    Fix bad reference to ctx

--- cogs/stats.py
+++ cogs/stats.py
@@ -15,7 +15,7 @@ class Stats:
     @commands.command()
     async def uptime(self, ctx):
         """Tells you how long the bot has been up for."""
-        await ctx.send(f'Uptime: {self.get_bot_uptime()}')
+        await ctx.send(f'Uptime: {self.get_bot_uptime(ctx.bot)}')
 
     @commands.command(aliases=['stats'])
     async def about(self, ctx):
@@ -23,19 +23,21 @@ class Stats:
 
         embed = discord.Embed()
 
+        bot = ctx.bot
+
         try:
             self.owner
         except AttributeError:
-            self.owner = ctx.bot.get_user(140293604726800385)
+            self.owner = bot.get_user(140293604726800385)
 
         embed.set_author(name=str(self.owner), icon_url=self.owner.avatar_url)
 
-        total_members = sum(len(s.members) for s in ctx.bot.guilds)
-        unique_members = len(ctx.bot.users)
+        total_members = sum(len(s.members) for s in bot.guilds)
+        unique_members = len(bot.users)
 
         voice = 0
         text = 0
-        for channel in ctx.bot.get_all_channels():
+        for channel in bot.get_all_channels():
             if isinstance(channel, discord.TextChannel):
                 text += 1
             else:
@@ -44,15 +46,15 @@ class Stats:
         members = f'{total_members} total\n{unique_members} unique'
         embed.add_field(name='Members', value=members)
         embed.add_field(name='Channels', value=f'{text} text\n{voice} voice')
-        embed.add_field(name='Uptime', value=self.get_bot_uptime(brief=True))
+        embed.add_field(name='Uptime', value=self.get_bot_uptime(bot, True))
         embed.set_footer(text='Made with discord.py',
                          icon_url='http://i.imgur.com/5BFecvA.png')
         try:
-            embed.timestamp = ctx.bot.uptime
+            embed.timestamp = bot.uptime
         except AttributeError:
             pass
 
-        embed.add_field(name='Guilds', value=len(ctx.bot.guilds))
+        embed.add_field(name='Guilds', value=len(bot.guilds))
 
         cpu_usage = self.process.cpu_percent()
         memory_usage = self.process.memory_full_info().uss / 2 ** 20
@@ -60,10 +62,10 @@ class Stats:
         embed.add_field(name='Memory Usage', value=f'{memory_usage:.2f} MiB')
         await ctx.send(embed=embed)
 
-    def get_bot_uptime(self, *, brief=False):
+    def get_bot_uptime(self, bot, brief=False):
         now = datetime.datetime.utcnow()
         try:
-            delta = now - ctx.bot.uptime
+            delta = now - bot.uptime
         except AttributeError:
             delta = datetime.timedelta()
         hours, remainder = divmod(int(delta.total_seconds()), 3600)
