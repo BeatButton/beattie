c747f8a94e5653a6030246f28b307eb2e9ab279b
BeatButton <thebeatbutton@gmail.com>
2017-07-27 19:37:31 -0600

    add Union

--- utils/converters.py
+++ utils/converters.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import re
 from time import mktime
 
 from discord.ext.commands import BadArgument, Converter
@@ -15,3 +14,21 @@ class Time(Converter):
         if code == 0:
             raise BadArgument
         return datetime.fromtimestamp(mktime(time))
+
+
+class Union(Converter):
+    def __init__(self, *converters):
+        self.converters = converters
+
+    async def convert(self, ctx, arg):
+        for converter in self.converters:
+            try:
+                return await ctx.command.do_conversion(ctx, converter, arg)
+            except (BadArgument, ValueError, TypeError):
+                pass
+        raise BadArgument('Conversion failed for {!r}'.format(self))
+
+    def __repr__(self):
+        names = (getattr(converter, '__name__', type(converter).__name__)
+                 for converter in self.converters)  
+        return '{}({})'.format(type(self).__name__, ', '.join(names))
