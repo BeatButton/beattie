93732fe13af04d8226f7cd1866d6c6442c5b4998
Mitchell Ferree <thebeatbutton@gmail.com>
2017-08-01 11:27:20 -0600

    Actually allow for removal of default_channel

--- utils/etc.py
+++ utils/etc.py
@@ -8,3 +8,8 @@ def reverse_insort(seq, val, lo=0, hi=None):
         else:
             lo = mid + 1
     seq.insert(lo, val)
+
+
+def default_channel(member):
+    return next((channel for channel in member.guild.text_channels
+                 if channel.permissions_for(member).read_messages), None)
