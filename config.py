d6104a8539193bfa05f799ec969639223800801b
Mitchell Ferree <thebeatbutton@gmail.com>
2017-12-16 08:59:32 -0700

    Attempt to give default settings to new guilds

--- config.py
+++ config.py
@@ -24,7 +24,9 @@ class Config:
                 query = s.select(Guild).where(Guild.id == gid)
                 guild = await query.first()
             if guild is None:
-                res = {'id': gid}
+                res = {col.name: col.type.create_default()
+                       for col in Guild.iter_columns()}
+                res['id'] = gid
             else:
                 res = to_dict(guild)
             self._cache[gid] = res
