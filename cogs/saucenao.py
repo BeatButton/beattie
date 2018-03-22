b57aef576c4f543bec72c0e6959c018afeeb986c
BeatButton <thebeatbutton@gmail.com>
2018-03-21 22:17:40 -0600

    Send typing in command

--- cogs/saucenao.py
+++ cogs/saucenao.py
@@ -11,31 +11,31 @@ class SauceNao:
 
     @commands.command(aliases=['sauce'])
     async def saucenao(self, ctx, *, link=''):
-        if not link:
-            if len(ctx.message.attachments) == 1:
-                link = ctx.message.attachments[0].url
-            else:
+        async with ctx.typing():
+            if not link:
+                if len(ctx.message.attachments) == 1:
+                    link = ctx.message.attachments[0].url
+                else:
+                    raise commands.BadArgument
+            elif ctx.message.attachments:
                 raise commands.BadArgument
-        elif ctx.message.attachments:
-            raise commands.BadArgument
-
-        link = link.strip('<>')
-        payload = {'url': link}
-
-        async with self.session.post(self.sauce_url, data=payload) as resp:
-            root = etree.fromstring(await resp.text(), self.parser)
-
-        results = root.xpath('.//div[@class="resultcontentcolumn"]/a')
-        sim_percent = 0
-        if results:
-            similarity = root.find(".//div[@class='resultsimilarityinfo']").text
-            sim_percent = float(similarity[:-1])
-
-        if not results or sim_percent <= 60:
-            await ctx.send('No sauce found.')
-        else:
-            await ctx.send(f'Sauce found ({similarity}) <{results[0].get("href")}>')
-
+    
+            link = link.strip('<>')
+            payload = {'url': link}
+    
+            async with self.session.post(self.sauce_url, data=payload) as resp:
+                root = etree.fromstring(await resp.text(), self.parser)
+    
+            results = root.xpath('.//div[@class="resultcontentcolumn"]/a')
+            sim_percent = 0
+            if results:
+                similarity = root.find(".//div[@class='resultsimilarityinfo']").text
+                sim_percent = float(similarity[:-1])
+    
+            if not results or sim_percent <= 60:
+                await ctx.send('No sauce found.')
+            else:
+                await ctx.send(f'Sauce found ({similarity}) <{results[0].get("href")}>')
 
     @saucenao.error
     async def saucenao_error(self, ctx, e):
