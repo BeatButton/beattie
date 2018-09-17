fdacd4538544c88e8c0103ab85a87562b2830382
BeatButton <thebeatbutton@gmail.com>
2018-09-16 18:13:48 -0600

    Use the paginator

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -3,6 +3,7 @@ from asyncjisho import Jisho
 import discord
 from discord.ext import commands
 
+from utils.paginator import Paginator
 
 class Dictionary:
     jisho_url = 'http://jisho.org/search/{}'
@@ -19,19 +20,23 @@ class Dictionary:
         if not data:
             await ctx.send('No words found.')
             return
-        res = data[0]
-        res = {k: '\n'.join(v) for k, v in res.items()}
-        res['english'] = ', '.join(res['english'].split('\n'))
-        embed = discord.Embed()
-        embed.url = self.jisho_url.format('%20'.join(keywords.split()))
-        embed.title = keywords
-        res = {k: self.format(v) for k, v in res.items()}
-        embed.add_field(name='Words', value=res['words'])
-        embed.add_field(name='Readings', value=res['readings'])
-        embed.add_field(name='Parts of Speech', value=res['parts_of_speech'])
-        embed.add_field(name='Meanings', value=res['english'])
-        embed.color = discord.Color(0x56d926)
-        await ctx.send(embed=embed)
+        results = []
+        size = len(data)
+        for i, res in enumerate(data, 1):
+            res = {k: '\n'.join(v) or 'None' for k, v in res.items()}
+            res['english'] = ', '.join(res['english'].split('\n'))
+            embed = discord.Embed()
+            embed.url = self.jisho_url.format('%20'.join(keywords.split()))
+            embed.title = keywords
+            embed.add_field(name='Words', value=res['words'])
+            embed.add_field(name='Readings', value=res['readings'])
+            embed.add_field(name='Parts of Speech', value=res['parts_of_speech'])
+            embed.add_field(name='Meanings', value=res['english'])
+            embed.set_footer(text="Page {}/{}".format(i, size))
+            embed.color = discord.Color(0x56d926)
+            results.append(embed)
+        paginator = Paginator(ctx, results)
+        await paginator.run()
 
     @commands.command(aliases=['ud', 'urban', 'urbandict'])
     async def urbandictionary(self, ctx, *, word):
@@ -41,20 +46,23 @@ class Dictionary:
         async with ctx.typing(), get(self.urban_url, params=params) as resp:
                 data = await resp.json()
         try:
-            res = data['list'][0]
+            results = data['list']
+            results[0]
         except IndexError:
             await ctx.send('Word not found.')
         else:
-            embed = discord.Embed()
-            embed.title = res['word']
-            embed.url = res['permalink']
-            embed.description = res['definition']
-            embed.color = discord.Color(0xe86222)
-            await ctx.send(embed=embed)
-
-    @staticmethod
-    def format(val):
-        return val if val else 'None'
+            embeds = []
+            size = len(results)
+            for i, res in enumerate(results, 1):
+                embed = discord.Embed()
+                embed.title = res['word']
+                embed.url = res['permalink']
+                embed.description = res['definition']
+                embed.color = discord.Color(0xe86222)
+                embed.set_footer(text='Page {}/{}'.format(i, size))
+                embeds.append(embed)
+            paginator = Paginator(ctx, embeds)
+            await paginator.run()
 
 
 def setup(bot):
