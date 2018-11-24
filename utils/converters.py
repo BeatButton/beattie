fcfb2663bf916c9d4a659d6bf553b7346e841fe6
BeatButton <thebeatbutton@gmail.com>
2018-11-23 21:54:15 -0700

    Use new Union converter

--- utils/converters.py
+++ utils/converters.py
@@ -14,21 +14,3 @@ class Time(Converter):
         if code == 0:
             raise BadArgument
         return datetime.fromtimestamp(mktime(time))
-
-
-class Union(Converter):
-    def __init__(self, *converters):
-        self.converters = converters
-
-    async def convert(self, ctx, arg):
-        for converter in self.converters:
-            try:
-                return await ctx.command.do_conversion(ctx, converter, arg)
-            except (BadArgument, ValueError, TypeError):
-                pass
-        raise BadArgument('Conversion failed for {!r}'.format(self))
-
-    def __repr__(self):
-        names = (getattr(converter, '__name__', type(converter).__name__)
-                 for converter in self.converters)  
-        return '{}({})'.format(type(self).__name__, ', '.join(names))
