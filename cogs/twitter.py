6268a73fd52ee2c335c10ecd7a9dcdda71563999
BeatButton <thebeatbutton@gmail.com>
2019-09-27 17:30:51 -0600

    Correct typo in regex

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -198,7 +198,7 @@ class Twitter(Cog):
         else:
             link = f"{link}&mode=medium"
         link = link.replace("http://", "https://")
-        illust_id = re.search(r"iillust_id=(\d+)|artworks/(\d+)", link).groups()[0]
+        illust_id = re.search(r"illust_id=(\d+)|artworks/(\d+)", link).groups()[0]
         headers = {
             "App-OS": "ios",
             "App-OS-Version": "10.3.1",
