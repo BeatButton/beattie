7e5bda1465736ea7e25e4f50af5d86cf52ca1208
BeatButton <thebeatbutton@gmail.com>
2020-03-29 17:41:11 -0600

    remove trailing whitespace

--- cogs/crosspost.py
+++ cogs/crosspost.py
@@ -150,7 +150,7 @@ class Crosspost(Cog):
         if (guild := message.guild) is None or message.author.bot:
             return
         if not guild.me.permissions_in(message.channel).send_messages:
-            return  
+            return
         if not (await self.bot.config.get_guild(guild.id)).get("crosspost_enabled"):
             return
         if "http" not in message.content:
