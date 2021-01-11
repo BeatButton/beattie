eab7dd6338661b66f99c86c7932e6854a2dbefeb
Mitchell Ferree <thebeatbutton@gmail.com>
2021-01-10 23:39:50 -0700

    Remove plonking (and therefore member and channel DB configuration)

--- schema/config.py
+++ schema/config.py
@@ -1,6 +1,6 @@
 from asyncqlio.orm.schema.column import Column
 from asyncqlio.orm.schema.table import table_base
-from asyncqlio.orm.schema.types import BigInt, Boolean, Text
+from asyncqlio.orm.schema.types import BigInt, Text
 
 Table = table_base()
 
@@ -10,15 +10,3 @@ class Guild(Table):  # type: ignore
     cog_blacklist = Column(Text, nullable=True)
     prefix = Column(Text, nullable=True)
     reminder_channel = Column(BigInt, nullable=True)
-
-
-class Member(Table):  # type: ignore
-    guild_id = Column(BigInt, primary_key=True)
-    id = Column(BigInt, primary_key=True)
-    plonked = Column(Boolean, nullable=True)
-
-
-class Channel(Table):  # type: ignore
-    id = Column(BigInt, primary_key=True)
-    guild_id = Column(BigInt, primary_key=True)
-    plonked = Column(Boolean, nullable=True)
