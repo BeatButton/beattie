d4e57f315f8c41827e5036d686328b3a94496df6
BeatButton <thebeatbutton@gmail.com>
2021-01-23 18:13:43 -0700

    Correctly type sent_bot_help signature

--- help.py
+++ help.py
@@ -1,12 +1,15 @@
+from typing import Mapping, Optional
+
 from discord.ext.commands import Cog, Command, MinimalHelpCommand
 
 
 class BHelp(MinimalHelpCommand):
-    async def send_bot_help(self, mapping: dict[Cog, list[Command]]):
+    async def send_bot_help(self, mapping: Mapping[Optional[Cog], list[Command]]):
         await super().send_bot_help(mapping)
-        await self.context.send(
-            "Join the support server for more help: discord.gg/a3kHCRs9Q8"
-        )
+        if ctx := self.context:
+            await ctx.send(
+                "Join the support server for more help: discord.gg/a3kHCRs9Q8"
+            )
 
     def add_subcommand_formatting(self, command: Command) -> None:
         fmt = "{0} \N{EN DASH} {1}" if command.short_doc else "{0}"
