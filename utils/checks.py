d8d26f16c966265452faf95e8389a0bed425bf6a
BeatButton <thebeatbutton@gmail.com>
2022-03-14 16:19:13 -0600

    Fix is_owner_or for threads

--- utils/checks.py
+++ utils/checks.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Callable, TypeVar
 
-from discord import Member, TextChannel
+from discord import Member, TextChannel, Thread
 from discord.ext import commands
 from discord.ext.commands import Context
 
@@ -13,9 +13,10 @@ def is_owner_or(**perms: bool) -> Callable[[T], T]:
     async def predicate(ctx: Context) -> bool:
         author = ctx.author
         channel = ctx.channel
-        if not isinstance(channel, TextChannel):
+        if ctx.guild is None:
             return True
         assert isinstance(author, Member)
+        assert isinstance(channel, (TextChannel, Thread))
         if await ctx.bot.is_owner(author):
             return True
         permissions = channel.permissions_for(author)
