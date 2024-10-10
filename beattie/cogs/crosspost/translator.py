8269e76cd9c66849083d53ea24f6311c1b0ec14a
BeatButton <thebeatbutton@gmail.com>
2024-10-09 20:08:34 -0600

    use lingua to detect languages
    also use a Task for language fetching

--- beattie/cogs/crosspost/translator.py
+++ beattie/cogs/crosspost/translator.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import asyncio
 import logging
 from abc import ABC, abstractmethod
 from collections import namedtuple
 from typing import TYPE_CHECKING, Mapping
+
+import lingua
 
 from beattie.utils.exceptions import ResponseError
 
@@ -20,14 +23,14 @@ ENGLISH = Language("en", "English (default)")
 class Translator(ABC):
     cog: Crosspost
     api_url: str
-    _languages: dict[str, Language]
+    _lang_task = asyncio.Task[dict[str, Language]] | None
 
     def __init__(self, cog: Crosspost, api_url: str, api_key: str):
         self.cog = cog
         self.api_url = api_url
         self.api_key = api_key
         self.logger = logging.getLogger(f"{__name__}.{type(self).__name__}")
-        self._languages = {}
+        self._lang_task = None
 
     @abstractmethod
     async def languages(self) -> Mapping[str, Language]: ...
@@ -42,34 +45,46 @@ class Translator(ABC):
 class HybridTranslator(Translator):
     libre: LibreTranslator
     deepl: DeeplTranslator
+    detector: lingua.LanguageDetector
 
     def __init__(self, cog: Crosspost, libre: LibreTranslator, deepl: DeeplTranslator):
         super().__init__(cog, "", "")
         self.libre = libre
         self.deepl = deepl
 
-    async def languages(self) -> Mapping[str, Language]:
-        if not self._languages:
-            deepl_langs = await self.deepl.languages()
-            libre_langs = await self.libre.languages()
-            shared = set(deepl_langs) & set(libre_langs)
-            self._languages = {code: deepl_langs[code] for code in shared}
+    async def _languages(self) -> Mapping[str, Language]:
 
-        return self._languages
+        deepl_langs = await self.deepl.languages()
+        libre_langs = await self.libre.languages()
+        shared = set(deepl_langs) & set(libre_langs) - {"xx", "zz"}
+
+        self.detector = lingua.LanguageDetectorBuilder.from_iso_codes_639_1(
+            *(getattr(lingua.IsoCode639_1, code.upper()) for code in shared)
+        ).build()
+
+        return {
+            "xx": DONT,
+            "zz": UNKNOWN,
+            **{code: libre_langs[code] for code in shared},
+        }
+
+    def languages(self) -> asyncio.Task[Mapping[str, Language]]:
+        if self._lang_task is None:
+            self._lang_task = asyncio.Task(self._languages())
+        return self._lang_task
 
     async def detect(self, text: str) -> Language:
         langs = await self.languages()
-        lang = await self.libre.detect(text)
 
-        if lang.code not in langs:
+        if lang := await asyncio.to_thread(self.detector.detect_language_of, text):
+            return langs[lang.iso_code_639_1.name.lower()]
+        else:
             return DONT
 
-        return lang
-
     async def translate(self, text: str, source: str, target: str) -> str:
+        langs = await self.languages()
         if source == "zz":
             source = (await self.detect(text)).code
-        langs = await self.languages()
         if source == DONT or source not in langs:
             return text
         if source in ("ja", "zh", "ko"):
@@ -81,23 +96,25 @@ class HybridTranslator(Translator):
 
 
 class LibreTranslator(Translator):
-    async def languages(self) -> Mapping[str, Language]:
-        if not self._languages:
-            self.logger.info("fetching language list")
-            body = {
-                "api_key": self.api_key,
-            }
-            async with self.cog.session.get(
-                f"{self.api_url}/languages", data=body
-            ) as resp:
-                data = await resp.json()
 
-            self._languages = {
-                lang["code"]: Language(lang["code"], lang["name"]) for lang in data
-            }
-            self._languages["xx"] = DONT
+    async def _languages(self) -> Mapping[str, Language]:
+        self.logger.info("fetching language list")
+        body = {
+            "api_key": self.api_key,
+        }
+        async with self.cog.session.get(f"{self.api_url}/languages", data=body) as resp:
+            data = await resp.json()
 
-        return self._languages
+        return {
+            "xx": DONT,
+            "zz": UNKNOWN,
+            **{lang["code"]: Language(lang["code"], lang["name"]) for lang in data},
+        }
+
+    def languages(self) -> asyncio.Task[Mapping[str, Language]]:
+        if self._lang_task is None:
+            self._lang_task = asyncio.Task(self._languages())
+        return self._lang_task
 
     async def detect(self, text: str) -> Language:
         self.logger.debug(f"detecting language for: {text}")
@@ -146,31 +163,35 @@ class DeeplTranslator(Translator):
         super().__init__(cog, api_url, api_key)
         self.headers = {"Authorization": f"DeepL-Auth-Key {api_key}"}
 
-    async def languages(self) -> Mapping[str, Language]:
-        if not self._languages:
-            self.logger.info("fetching language list")
-            async with self.cog.session.get(
-                f"{self.api_url}/languages",
-                headers=self.headers,
-                params={"type": "target"},
-            ) as resp:
-                data = await resp.json()
+    async def _languages(self) -> Mapping[str, Language]:
+        self.logger.info("fetching language list")
+        async with self.cog.session.get(
+            f"{self.api_url}/languages",
+            headers=self.headers,
+            params={"type": "target"},
+        ) as resp:
+            data = await resp.json()
 
-            self._languages = {
-                (code := lang["language"].lower()): Language(code, lang["name"])
-                for lang in data
-            }
-            for code, pref, drop in [
-                ("en", "us", "gb"),
-                ("pt", "br", "pt"),
-            ]:
-                self._languages.pop(f"{code}-{drop}")
-                lang = self._languages.pop(f"{code}-{pref}")
-                self._languages[code] = Language(code, lang.name)
-            self._languages["xx"] = DONT
-            self._languages["zz"] = UNKNOWN
+        langs = {
+            (code := lang["language"].lower()): Language(code, lang["name"])
+            for lang in data
+        }
+        for code, pref, drop in [
+            ("en", "us", "gb"),
+            ("pt", "br", "pt"),
+        ]:
+            langs.pop(f"{code}-{drop}")
+            lang = langs.pop(f"{code}-{pref}")
+            langs[code] = Language(code, lang.name)
+        langs["xx"] = DONT
+        langs["zz"] = UNKNOWN
 
-        return self._languages
+        return langs
+
+    def languages(self) -> asyncio.Task[Mapping[str, Language]]:
+        if self._lang_task is None:
+            self._lang_task = asyncio.Task(self._languages())
+        return self._lang_task
 
     async def detect(self, text: str) -> Language:
         return UNKNOWN
