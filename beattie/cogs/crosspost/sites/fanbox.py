e51583e1af41baca2a30ca146cb7fd6bcac7a317
BeatButton <thebeatbutton@gmail.com>
2025-07-20 22:27:15 -0600

    maybe fix fanbox!

--- beattie/cogs/crosspost/sites/fanbox.py
+++ beattie/cogs/crosspost/sites/fanbox.py
@@ -1,14 +1,21 @@
 from __future__ import annotations
 
+import json
+import logging
 import re
-from types import MappingProxyType
-from typing import TYPE_CHECKING
+import urllib.parse
+from typing import TYPE_CHECKING, Any
 
 import httpx
+import toml
+from lxml import etree
 
 from .site import Site
 
 if TYPE_CHECKING:
+    from types import TracebackType
+
+    from ..cog import Crosspost
     from ..context import CrosspostContext
     from ..queue import FragmentQueue
 
@@ -18,13 +25,22 @@ class Fanbox(Site):
     pattern = re.compile(
         r"https://(?:(?:www\.)?fanbox\.cc/@)?([\w-]+)(?:\.fanbox\.cc)?/posts/(\d+)",
     )
+    solver_url: str
+    proxy_url: str
 
-    headers = MappingProxyType(
-        {
-            "Accept": "application/json, text/plain, */*",
-            "Origin": "https://www.fanbox.cc",
-        },
-    )
+    def __init__(self, cog: Crosspost):
+        super().__init__(cog)
+        try:
+            with open("config/crosspost/fanbox.toml") as fp:
+                data = toml.load(fp)
+                self.solver_url = data["solver"]
+                self.proxy_url = data["proxy"]
+        except (FileNotFoundError, KeyError):
+            logging.getLogger(__name__).warning(
+                "no solver/proxy setup found, fanbox disabled.",
+            )
+            self.cog.sites.remove(self)
+        self.parser = etree.HTMLParser()
 
     async def handler(
         self,
@@ -35,17 +51,18 @@ class Fanbox(Site):
     ):
         queue.link = f"https://www.fanbox.cc/@{user}/posts/{post_id}"
         url = f"https://api.fanbox.cc/post.info?postId={post_id}"
-        headers = {**self.headers, "Referer": queue.link}
-        async with (
-            httpx.AsyncClient(follow_redirects=True, timeout=None) as sess,
-            self.cog.get(
+
+        async with FlareSolverr(self.solver_url, self.proxy_url) as fs:
+            resp = await fs.get(
                 url,
-                headers=headers,
-                session=sess,
-                use_browser_ua=True,
-            ) as resp,
-        ):
-            data = resp.json()
+                headers={
+                    "Accept": "application/json, text/plain, */*",
+                    "Origin": "https://www.fanbox.cc",
+                },
+            )
+
+        root = etree.fromstring(resp["solution"]["response"], self.parser)
+        data = json.loads(root.xpath("//pre")[0].text)
 
         post = data["body"]
         body = post["body"]
@@ -53,6 +70,7 @@ class Fanbox(Site):
             return
 
         queue.author = post["creatorId"]
+        headers = {"Referer": queue.link}
 
         match post["type"]:
             case "image":
@@ -96,3 +114,58 @@ class Fanbox(Site):
             case other:
                 msg = f"Unrecognized post type {other}!"
                 raise RuntimeError(msg)
+
+
+class FlareSolverr:
+    solver: str
+    proxy: str
+    session: str
+    client: httpx.AsyncClient
+
+    def __init__(self, solver: str, proxy: str):
+        self.solver = solver
+        self.proxy = proxy
+        self.client = httpx.AsyncClient(follow_redirects=True, timeout=None)
+
+    async def __aenter__(self):
+        data = await self._request(
+            {"cmd": "sessions.create", "proxy": {"url": self.proxy}},
+        )
+        self.session = data["session"]
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ):
+        if self.session is not None:
+            await self._request({"cmd": "sessions.destroy", "session": self.session})
+
+    async def _request(self, command: dict[str, Any]) -> dict[str, Any]:
+        resp = await self.client.post(
+            self.solver,
+            headers={"Content-Type": "application/json"},
+            content=json.dumps(command),
+        )
+        data = resp.json()
+        if data["status"] != "ok":
+            msg = f"solver error: {data['message']}"
+            raise RuntimeError(msg)
+
+        return data
+
+    async def get(self, url: str, *, headers: dict[str, str] = None) -> dict[str, Any]:
+        if headers:
+            for name, value in headers.items():
+                slug = f"{name}:{value}"
+                data = urllib.parse.quote(slug, safe="")
+                url = f"{url}&$$headers[]={data}"
+        return await self._request(
+            {
+                "cmd": "request.get",
+                "url": url,
+                "session": self.session,
+            },
+        )
