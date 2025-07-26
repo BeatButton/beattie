4cbf3c36c8c546575a9527f983b221a3ea737ff9
BeatButton <thebeatbutton@gmail.com>
2025-07-26 11:32:12 -0600

    remove page title text

--- beattie/cogs/crosspost/sites/tiktok.py
+++ beattie/cogs/crosspost/sites/tiktok.py
@@ -15,14 +15,13 @@ if TYPE_CHECKING:
 class Tiktok(Site):
     name = "tiktok"
     pattern = re.compile(
-        r"(https?://(?:www\.)(?:vx|kk)?tiktok\.com/(?:@[\w\.]+/video/|t/)(\w+))",
+        r"https?://(?:www\.)(?:vx|kk)?tiktok\.com/(?:@[\w\.]+/video/|t/)(\w+)",
     )
 
     async def handler(
         self,
         _ctx: CrosspostContext,
         queue: FragmentQueue,
-        link: str,
         video_id: str,
     ):
         async with self.cog.get(f"https://kktiktok.com/t/{video_id}?_kk=1") as resp:
@@ -31,9 +30,3 @@ class Tiktok(Site):
         filename = f"{video_id}.mp4"
 
         queue.push_file(url, filename=filename)
-
-        async with self.cog.get(link, use_browser_ua=True) as resp:
-            root = html.document_fromstring(resp.content, self.cog.parser)
-
-        text: str = root.find(".//title").text
-        queue.push_text(text.removeprefix("TikTok - "))
