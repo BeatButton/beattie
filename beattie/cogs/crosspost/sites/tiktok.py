ed8af5d5b85786f647b160fcd06f4f70a5e71791
BeatButton <thebeatbutton@gmail.com>
2025-07-26 11:00:00 -0600

    fix tiktok

--- beattie/cogs/crosspost/sites/tiktok.py
+++ beattie/cogs/crosspost/sites/tiktok.py
@@ -5,7 +5,6 @@ from typing import TYPE_CHECKING
 
 from lxml import html
 
-from .selectors import OG_DESCRIPTION, OG_VIDEO
 from .site import Site
 
 if TYPE_CHECKING:
@@ -16,33 +15,25 @@ if TYPE_CHECKING:
 class Tiktok(Site):
     name = "tiktok"
     pattern = re.compile(
-        r"https?://(?:www\.)(?:vx)?tiktok\.com/(?:@[\w\.]+/video/\d+|t/\w+)+",
+        r"(https?://(?:www\.)(?:vx)?tiktok\.com/(?:@[\w\.]+/video/|t/)(\w+))",
     )
 
-    async def handler(self, _ctx: CrosspostContext, queue: FragmentQueue, link: str):
-        if "vxtiktok.com" not in link:
-            link = link.replace("tiktok.com", "vxtiktok.com")
+    async def handler(
+        self,
+        _ctx: CrosspostContext,
+        queue: FragmentQueue,
+        link: str,
+        video_id: str,
+    ):
+        async with self.cog.get(f"https://kktiktok.com/t/{video_id}?_kk=1") as resp:
+            url = resp.json()["url"]
 
-        async with self.cog.get(
-            link,
-            follow_redirects=False,
-            headers={"User-Agent": "test"},
-        ) as resp:
-            root = html.document_fromstring(resp.content, self.cog.parser)
-
-        try:
-            url = root.xpath(OG_VIDEO)[0].get("content")
-        except IndexError:
-            msg = "no video in vxtiktok response"
-            raise RuntimeError(msg) from None
-
-        async with self.cog.get(url, method="HEAD") as resp:
-            pass
-
-        video_id = url.rpartition("/")[2]
         filename = f"{video_id}.mp4"
 
         queue.push_file(url, filename=filename)
 
-        desc = root.xpath(OG_DESCRIPTION)[0].get("content")
-        queue.push_text(desc)
+        async with self.cog.get(link, use_browser_ua=True) as resp:
+            root = html.document_fromstring(resp.content, self.cog.parser)
+
+        text: str = root.find(".//title").text
+        queue.push_text(text.removeprefix("TikTok - "))
