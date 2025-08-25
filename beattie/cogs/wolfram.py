a98cdbb931ee1364499053ca90be98f5b0c367c4
BeatButton <thebeatbutton@gmail.com>
2025-08-25 16:11:22 -0600

    refactor wolfram config

--- beattie/cogs/wolfram.py
+++ beattie/cogs/wolfram.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypedDict
 
 import toml
 from lxml import etree
@@ -14,22 +14,27 @@ if TYPE_CHECKING:
     from beattie.bot import BeattieBot
     from beattie.context import BContext
 
+    class Config(TypedDict):
+        key: str
+
+
+API_URL = "http://api.wolframalpha.com/v2/query"
+
 
 class Wolfram(Cog):
     chars: Mapping[int, str] = {0xF74C: " d", 0xF74D: "e", 0xF74E: "i", 0xF7D9: " = "}
 
     def __init__(self):
-        self.url: str = "http://api.wolframalpha.com/v2/query"
-        with open("config/config.toml") as file:
-            data = toml.load(file)
-        self.key: str = data["wolfram_key"]
+        with open("config/wolfram.toml") as file:
+            data: Config = toml.load(file)  # pyright: ignore[reportAssignmentType]
+        self.key = data["key"]
 
     @commands.command(aliases=["wolf", "w"])
     async def wolfram(self, ctx: BContext, *, inp: str):
         """Query Wolfram|Alpha."""
         async with ctx.typing():
             params = {"input": inp, "appid": self.key, "format": "plaintext"}
-            async with ctx.bot.get(self.url, params=params) as resp:
+            async with ctx.bot.get(API_URL, params=params) as resp:
                 text = resp.text
             root = etree.fromstring(text.encode(), etree.XMLParser())
             interpret = root.xpath(
