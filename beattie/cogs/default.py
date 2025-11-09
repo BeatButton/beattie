4911b4180f5eea45ac17cb25b6131bfe95dc61e4
BeatButton <thebeatbutton@gmail.com>
2025-11-08 22:36:00 -0700

    ask for all permissions the bot might need

--- beattie/cogs/default.py
+++ beattie/cogs/default.py
@@ -91,7 +91,7 @@ class Default(Cog):
         """Get the invite for the bot."""
         url = (
             "<https://discordapp.com/oauth2/authorize?"
-            "client_id=1390347718227923107&scope=bot>"
+            "client_id=1390347718227923107&scope=bot&permissions=274878295104>"
         )
         await ctx.send(url.format(ctx.me.id))
 
