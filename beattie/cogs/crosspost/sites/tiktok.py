70954fc0d972a8c1fedaf2776cc5f240ffc072ba
BeatButton <thebeatbutton@gmail.com>
2026-04-20 12:28:57 -0600

    use tnktok for tiktok, support galleries

--- beattie/cogs/crosspost/sites/tiktok.py
+++ beattie/cogs/crosspost/sites/tiktok.py
@@ -1,11 +1,21 @@
 from __future__ import annotations
 
+import base64
 import re
+import urllib.parse as urlparse
+from html import unescape as html_unescape
 from typing import TYPE_CHECKING, TypedDict
 
-from beattie.utils.exceptions import ResponseError
+from lxml import etree
 
+from ..selectors import og
 from .site import Site
+
+OG_URL = og("url")
+OG_VIDEO = og("video")
+OG_IMAGE = og("image")
+OG_IMAGE_TYPE = og("image:type")
+LINK_OEMBED = './/link[@type="application/json+oembed"]'
 
 if TYPE_CHECKING:
     from ..context import CrosspostContext
@@ -18,24 +28,49 @@ if TYPE_CHECKING:
 class Tiktok(Site):
     name = "tiktok"
     pattern = re.compile(
-        r"https?://(?:\w+\.)?(?:vx|kk)?tiktok\.com/(?:@[\w\.]+/video/|t/)?([\w\-]+)",
+        r"https?://(?:\w+\.)*(?:vx|kk)?t[in]ktok\.com/(?:@[\w\.]+/video/|t/)?[\w\-]+",
     )
 
     async def handler(
         self,
         _ctx: CrosspostContext,
         queue: FragmentQueue,
-        video_id: str,
+        link: str,
     ):
-        link = f"https://kktiktok.com/t/{video_id}?_kk=1"
-        async with self.cog.get(link) as resp:
-            post: Response = resp.json()
+        split = urlparse.urlsplit(link)
+        link = f"https://tnktok.com{split.path}?addDesc=true"
+        async with self.cog.get(
+            link,
+            error_for_status=False,
+            follow_redirects=False,
+        ) as resp:
+            root = etree.fromstring(resp.content, self.cog.parser)
 
-        url = post["url"]
+        try:
+            queue.link = root.xpath(OG_URL)[0].get("content")
+        except IndexError:
+            msg = "Post not found."
+            raise IndexError(msg) from None
 
-        if url is None:
-            raise ResponseError(404, link)
+        alt = root.xpath(LINK_OEMBED)[0].get("href")
+        split = urlparse.urlsplit(urlparse.unquote(html_unescape(alt)))
+        query = urlparse.parse_qs(split.query)
+        queue.author = query["unique_id"][0]
 
-        filename = f"{video_id}.mp4"
+        if images := root.xpath(OG_IMAGE):
+            mimetypes = root.xpath(OG_IMAGE_TYPE)
+            post_id = images[0].get("content").rpartition("/")[2]
+            for idx, (image, mimetype) in enumerate(
+                zip(images, mimetypes, strict=True),
+                1,
+            ):
+                ext = mimetype.get("content").rpartition("/")[2]
+                filename = f"{post_id}_{idx}.{ext}"
+                queue.push_file(image.get("content"), filename=filename)
+        else:
+            url = root.xpath(OG_VIDEO)[0].get("content")
+            queue.push_file(url)
 
-        queue.push_file(url.partition("?")[0], filename=filename)
+        if encoded := query.get("description"):
+            text = base64.b64decode(encoded[0]).decode("utf-8")
+            queue.push_text(text)
